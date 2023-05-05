6bd56944166d174f96dcd541cebc8545081de3b0
scottmishra <scottmishra@gmail.com>
2023-05-05 03:05:54 +0000

    adding grobid connection

--- vectorize_pdf.py
+++ vectorize_pdf.py
@@ -2,5 +2,14 @@
 ## we want to read in the PDF
 ## Maybe see if it has sections
 ## Extract the section for Abastract and the Conclusions
-## Then do some research on vecotrization
+# ## Then do some research on vecotrizatiion
+## We will use GROBID and this scipy_pdf package and see whats going on
 
+path = "methods-based-on-wavelets-for-time-delay-estimation-of-ultrasoun.pdf"
+url="localhost"
+port="8071"
+
+import scipdf
+article_dict = scipdf.parse_pdf_to_dict(path) # return dictionary
+
+print(article_dict['abstract'])
