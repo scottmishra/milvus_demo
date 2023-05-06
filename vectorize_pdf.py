7dc3a602c24c990619c6c411d3e5741268afbca1
scottmishra <scottmishra@gmail.com>
2023-05-06 20:47:33 +0000

    Finalized inserting data vectors...need to figure out the embeddings, it looks like there are embeddings PER input row so...probably need to have a vector per col

--- vectorize_pdf.py
+++ vectorize_pdf.py
@@ -1,15 +1,66 @@
 ## Lets see
-## we want to read in the PDF
-## Maybe see if it has sections
-## Extract the section for Abastract and the Conclusions
+## we want to read in the PDF x
+## Maybe see if it has sections x
+## Extract the section for Abastract and the Conclusions x
 # ## Then do some research on vecotrizatiion
-## We will use GROBID and this scipy_pdf package and see whats going on
+## We will use GROBID and this scipy_pdf package and see whats going on x
 
 import scipdf
 import glob
+import openai
+from pymilvus import (
+    connections,
+    utility,
+    FieldSchema, CollectionSchema, DataType,
+    Collection,
+)
+
+COLLECTION_NAME = 'pdf_search'
+DIMENSION = 1536
+connections.connect("default", host="host.docker.internal", port="19530")
+
+# Remove collection if it already exists
+# if utility.has_collection(COLLECTION_NAME):
+#     utility.drop_collection(COLLECTION_NAME)
+
+# Create collection which includes the id, title, and embedding.
+fields = [
+    FieldSchema(name="id",dtype=DataType.INT64,is_primary=True, auto_id=True),
+    FieldSchema(name='title', dtype=DataType.VARCHAR, max_length=64000),
+    FieldSchema(name='abstract', dtype=DataType.VARCHAR, max_length=64000),
+    # FieldSchema(name='conclusion', dtype=DataType.VARCHAR, max_length=64000),
+    FieldSchema(name='authors', dtype=DataType.VARCHAR, max_length=64000),
+    # FieldSchema(name='pub_date', dtype=DataType.VARCHAR, max_length=128),
+    FieldSchema(name='embedding', dtype=DataType.FLOAT_VECTOR, dim=DIMENSION)
+]
+schema = CollectionSchema(fields=fields)
+collection = Collection(name=COLLECTION_NAME, schema=schema)
+INDEX_PARAM = {
+    'metric_type':'L2',
+    'index_type':"HNSW",
+    'params':{'M': 8, 'efConstruction': 64}
+}
+BATCH_SIZE = 1000
+# Create the index on the collection and load it.
+# collection.create_index(field_name="embedding", index_params=INDEX_PARAM)
+collection.load()
+
+## Setup ENV
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GPT_3_API_KEY = os.getenv('GPT_3_API_KEY')
+GPT_4_API_KEY = os.getenv('GPT_4_API_KEY')
+GPT_4_ORG = os.getenv('GPT_4_ORG')
 
 def processArticle(path):
-    return scipdf.parse_pdf_to_dict(path, grobid_url="http://host.docker.internal:8071") # return dictionary
+    try:
+        return scipdf.parse_pdf_to_dict(path, grobid_url="http://host.docker.internal:8071") # return dictionary
+    except:
+        print(f"Failed to process {path}")
+        return None
 
 def glob_folder(path): 
     files = glob.glob(path)
@@ -41,19 +92,64 @@ def collect_publish_data(globbed_files):
     data = []
     for file in globbed_files: ## TODO: use asyncio to push this into parallel runs to make this faster
         processed_article = processArticle(file)
-        data.append(processed_article)
+        if(processed_article == None or processed_article['abstract'] == ''):
+            continue
+        prepared_for_embedding = {
+            'title': processed_article['title'] if processed_article['title'] != "" else file.split("/")[-1],
+            'authors': processed_article['authors'] if processed_article['authors'] != "" else "Unknown",
+            'pub_date': processed_article['pub_date'],
+            'abstract': processed_article['abstract'],
+            'conclusion': check_for_conclusion(processed_article)
+        }
+        data.append(prepared_for_embedding)
     return data
+
+def check_for_conclusion(article):
+    check_conclusion = [x for x in article['sections'] if 'conclusion' in x['heading'].lower()]
+    if len(check_conclusion) > 0:
+        return check_conclusion[0]['text']
+    return ""
+
 
 def create_data_embedding(data):
     # Reach out to ChatGPT to get the embedding
+    embed_model = "text-embedding-ada-002"
+    results = []
+    for item in data:
+        embedding_result = openai.Embedding.create(
+            input=[
+            item['title'],
+            item['authors'],
+            item['abstract'],
+            # item['conclusion'],
+            # item['pub_date']
+        ], engine=embed_model
+        )
+        results.append(embedding_result['data'][2]['embedding'])
+    return results
+
+def push_into_milvus(data_points, embeddings):
+    for i in range(0,len(embeddings)):
+        data_points[i]['embedding'] = embeddings[i]
+    entities = [
+        [x['title'] for x in data_points],
+        [x['authors'] for x in data_points],
+        [x['abstract'] for x in data_points],
+        # [x['conclusion'] for x in data_points],
+        # [x['pub_date'] for x in data_points],
+        [x['embedding'] for x in data_points]
+    ]
+    collection.insert(entities)
     return
 
-def push_into_milvus(embedding):
-    # 
-    return
 
-path = "./PDFs/B/*" ## TODO: set this as an input parameter to help streamline the processing
 
+openai.organization = GPT_4_ORG
+openai.api_key = GPT_4_API_KEY
+openai.Model.list()
+
+path = "./PDFs/A/*" ## TODO: set this as an input parameter to help streamline the processing
 files = glob_folder(path)
-processed_data = collect_publish_data(files[0:3])
-print(processed_data)
+processed_data = collect_publish_data(files)
+embeddings = create_data_embedding(processed_data)
+push_into_milvus(processed_data, embeddings)
